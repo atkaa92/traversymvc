e2185f5e0c5f1c54df5552723ff6beed06812f64
Karen <atkaa92@gmail.com>
2018-05-14 02:55:15 +0500

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+  define('APPROOT', dirname(dirname(__FILE__))) ;
+  define('URLROOT', 'http://mvc.local/') ;
+  define('SITENAME', 'My MVC') ;
+
+  define('DB_HOST', 'localhost');
+	define('DB_USER', '_YOUR_USER_');
+	define('DB_PASS', '_YOUR_PASSWORD_');
+  define('DB_NAME', '_YOUR_DATABASE_');
